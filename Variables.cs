1faeaafa83a454c2c049d4bcfa0a64d21cb38ef4
Judannn <86912619+Judannn@users.noreply.github.com>
2024-04-13 13:25:48 +0800

    Update Variables.cs
    Removal of API keys

--- Variables.cs
+++ Variables.cs
@@ -5,9 +5,9 @@ namespace XFGoogleMapSample
     public static class Variables
     {
         // https://developers.google.com/maps/documentation/android-api/signup
-        public const string GOOGLE_MAPS_ANDROID_API_KEY = "AIzaSyA8f5Te5lW8RbA4WVInasD6RugVKYFju1E";
+        public const string GOOGLE_MAPS_ANDROID_API_KEY = "";
 
         // https://developers.google.com/maps/documentation/ios-sdk/start#step_4_get_an_api_key
-        public const string GOOGLE_MAPS_IOS_API_KEY = "your_ios_maps_apikey";
+        public const string GOOGLE_MAPS_IOS_API_KEY = "";
     }
 }
